98b75bbca933958b4268b9f9367079450e868e8c
Digvijay Singh <107340811+roblox-codex@users.noreply.github.com>
2023-02-21 00:42:34 +0530

    Create Addition.cs

---
+++ C#/Addition.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+ 
+ 
+namespace Add_Sub_Mul_Div
+{
+	
+class Program
+	  	{
+ 
+    	       	static void Main(string[] args)
+    	       	{
+ 
+       	              	 int a = 15, b = 10;
+ 
+        	             	Console.WriteLine("Addition = {0}", a + b);
+ 
+       	              	 Console.WriteLine("Subtraction = {0}", a - b);
+ 
+        	             	Console.WriteLine("Multiplication = {0}", a * b);
+ 
+        	             	Console.WriteLine("Division = {0}", (float)a / b);
+ 
+    	       	}
+ 
+      	 }
+ 
+ }
