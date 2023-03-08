370fb3e4a893f80e78170c54abed0ba311499024
Digvijay Singh <107340811+roblox-codex@users.noreply.github.com>
2023-03-08 14:50:57 +0530

    Create find_day.py

---
+++ Python/find_day.py
@@ -0,0 +1,13 @@
+import datetime
+
+# Take input from user
+date_str = input("Enter date (in dd/mm/yyyy format): ")
+
+# Convert input string to datetime object
+date_obj = datetime.datetime.strptime(date_str, '%d/%m/%Y')
+
+# Get day of the week as a string
+day = date_obj.strftime('%A')
+
+# Output the day
+print("The day is:", day)
