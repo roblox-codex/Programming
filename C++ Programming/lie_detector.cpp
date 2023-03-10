751a875b6b247e45e7f60b864aad2e9a1960ba63
Digvijay Singh <107340811+roblox-codex@users.noreply.github.com>
2023-03-10 16:34:18 +0530

    Update lie_detector.cpp

--- C++ Programming/lie_detector.cpp
+++ C++ Programming/lie_detector.cpp
@@ -3,7 +3,8 @@
 
 using namespace std;
 
-int main() {
+int main() 
+{
     string name;
     int age;
     bool isMarried;
@@ -16,9 +17,11 @@ int main() {
     cout << "Are you married? (1 for yes, 0 for no): ";
     cin >> isMarried;
     
-    if (age < 18) {
+    if (age < 18) 
+{
         cout << "You are too young to take this test." << endl;
-    } else {
+    } else 
+{
         cout << "Answer the following questions truthfully." << endl;
         
         string answer1, answer2;
@@ -26,19 +29,23 @@ int main() {
         
         cout << "Question 1: Have you ever stolen something?" << endl;
         cin >> answer1;
-        if (answer1 == "yes" && isMarried) {
+        if (answer1 == "yes" && isMarried) 
+{
             isLying = true;
         }
         
         cout << "Question 2: Have you ever lied to your spouse?" << endl;
         cin >> answer2;
-        if (answer2 == "yes" && !isMarried) {
+        if (answer2 == "yes" && !isMarried) 
+{
             isLying = true;
         }
         
-        if (isLying) {
+        if (isLying) 
+{
             cout << "You are lying!" << endl;
-        } else {
+        } else 
+{
             cout << "You are telling the truth." << endl;
         }
     }
