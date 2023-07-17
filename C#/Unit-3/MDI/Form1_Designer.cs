eb6c5ea3fa729993ca799ae6e69f316a2ad86f97
Digvijay Singh <107340811+roblox-codex@users.noreply.github.com>
2023-07-17 22:56:13 +0530

    Create Form1_Designer.cs

---
+++ C#/Unit-3/MDI/Form1_Designer.cs
@@ -0,0 +1,80 @@
+namespace MDIApplication
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openChildFormToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openDialogToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openChildFormToolStripMenuItem,
+            this.openDialogToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openChildFormToolStripMenuItem
+            // 
+            this.openChildFormToolStripMenuItem.Name = "openChildFormToolStripMenuItem";
+            this.openChildFormToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openChildFormToolStripMenuItem.Text = "Open Child Form";
+            this.openChildFormToolStripMenuItem.Click += new System.EventHandler(this.openChildFormToolStripMenuItem_Click);
+            // 
+            // openDialogToolStripMenuItem
+            // 
+            this.openDialogToolStripMenuItem.Name = "openDialogToolStripMenuItem";
+            this.openDialogToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openDialogToolStripMenuItem.Text = "Open Dialog";
+            this.openDialogToolStripMenuItem.Click += new System.EventHandler(this.openDialogToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "MDI Application";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openChildFormToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openDialogToolStripMenuItem;
+    }
+}
