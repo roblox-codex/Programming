38cc345634ce80647c070270a4e4952ff8920308
Digvijay Singh <107340811+roblox-codex@users.noreply.github.com>
2023-08-09 23:46:07 +0530

    Create swap.py
    Swapping without using third variable

---
+++ Python/swap.py
@@ -0,0 +1,10 @@
+a = float(input("Enter first number a:- "))
+b = float(input("Enter second number b:- "))
+
+a = a + b
+b = a - b
+a = a - b
+
+print("After swapping")
+print("First number a is:- ", a)
+print("Second number b is:- ", b)
