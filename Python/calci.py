fbdcd722a8b974a943ced61d3f2cdf9160c86ab8
Digvijay Singh <107340811+roblox-codex@users.noreply.github.com>
2023-08-14 23:30:06 +0530

    Create calci.py

---
+++ Python/calci.py
@@ -0,0 +1,31 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        print("Cannot divide by zero!")
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+operator = input("Enter operator (+, -, *, /): ")
+
+if operator == "+":
+    result = add(num1, num2)
+elif operator == "-":
+    result = subtract(num1, num2)
+elif operator == "*":
+    result = multiply(num1, num2)
+elif operator == "/":
+    result = divide(num1, num2)
+else:
+    print("Invalid operator!")
+
+print("Result:", result)
