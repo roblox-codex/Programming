e1001fbc7578eeeb29e3ccf6ba0bb8cb75504d84
Digvijay Singh <107340811+roblox-codex@users.noreply.github.com>
2023-08-28 23:45:59 +0530

    Create unary.py

---
+++ Python/unary.py
@@ -0,0 +1,18 @@
+# Unary minus (-) operator
+num1 = 10
+num2 = -num1
+print("Unary Minus Operator:", num2)
+
+# Unary plus (+) operator (not very common, as it doesn't change the value)
+num3 = +num1
+print("Unary Plus Operator:", num3)
+
+# Bitwise NOT (~) operator
+bit_value = 0b101010
+bit_complement = ~bit_value
+print("Bitwise NOT Operator:", bin(bit_complement))
+
+# Logical NOT (not) operator
+bool_value = True
+bool_negated = not bool_value
+print("Logical NOT Operator:", bool_negated)
