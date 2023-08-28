923b9f85d14585f89d8cad2d996f871e028083be
Digvijay Singh <107340811+roblox-codex@users.noreply.github.com>
2023-08-28 23:51:01 +0530

    Create relational_operator.py

---
+++ Python/relational_operator.py
@@ -0,0 +1,27 @@
+# Relational operators demonstration
+
+# Greater than (>)
+num1 = 10
+num2 = 5
+greater_than_result = num1 > num2
+print(f"{num1} > {num2}:", greater_than_result)
+
+# Less than (<)
+less_than_result = num1 < num2
+print(f"{num1} < {num2}:", less_than_result)
+
+# Greater than or equal to (>=)
+greater_than_equal_result = num1 >= num2
+print(f"{num1} >= {num2}:", greater_than_equal_result)
+
+# Less than or equal to (<=)
+less_than_equal_result = num1 <= num2
+print(f"{num1} <= {num2}:", less_than_equal_result)
+
+# Equal to (==)
+equal_to_result = num1 == num2
+print(f"{num1} == {num2}:", equal_to_result)
+
+# Not equal to (!=)
+not_equal_result = num1 != num2
+print(f"{num1} != {num2}:", not_equal_result)
