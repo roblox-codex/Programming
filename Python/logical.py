556f3e5f33d73b9fcb55e0429c23c5a296fa6b92
Digvijay Singh <107340811+roblox-codex@users.noreply.github.com>
2023-08-31 22:24:48 +0530

    Create logical.py

---
+++ Python/logical.py
@@ -0,0 +1,9 @@
+# Logical operation which takes user input
+
+x = float(input("Enter value of x:- "))
+y = float(input("Enter value of y:- "))
+
+print("AND Operation:- ", x and y)
+print("OR Operation:- ", x or y)
+print("NOT Operation:- ", not x)
+print("NOT Operation:- ", not y)
