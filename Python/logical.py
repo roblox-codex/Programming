e9015387c2f3d0559b4f5f48cd5c359a00196fbd
Digvijay Singh <107340811+roblox-codex@users.noreply.github.com>
2023-08-31 22:30:10 +0530

    Update logical.py

--- Python/logical.py
+++ Python/logical.py
@@ -3,7 +3,15 @@
 x = float(input("Enter value of x:- "))
 y = float(input("Enter value of y:- "))
 
+# AND Operator
+# If x is False, it returns x, otherwise it returns y
 print("AND Operation:- ", x and y)
+
+# OR Operator
+# If x is False, it returns y, otherwise it returns x
 print("OR Operation:- ", x or y)
+
+# NOT Operator
+# If x is False, it returns True, otherwise True
 print("NOT Operation:- ", not x)
-print("NOT Operation:- ", not y)
+
