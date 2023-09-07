b717b3793f355e5fdb07dcec6da015ecd67443a1
Digvijay Singh <107340811+roblox-codex@users.noreply.github.com>
2023-09-07 22:29:25 +0530

    Create floor_ceil.py

---
+++ Python/floor_ceil.py
@@ -0,0 +1,10 @@
+import math
+
+number = float(input("Enter a floating-point number: "))
+
+floor_result = math.floor(number)
+ceil_result = math.ceil(number)
+
+print(f"Original number: {number}")
+print(f"Floor: {floor_result}")
+print(f"Ceil: {ceil_result}")
