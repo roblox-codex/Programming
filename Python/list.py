074c791318417b5762cd42ce7044d57acfe077b7
Digvijay Singh <107340811+roblox-codex@users.noreply.github.com>
2023-09-09 22:38:29 +0530

    Update list.py

--- Python/list.py
+++ Python/list.py
@@ -10,3 +10,14 @@ def swapList(newList):
 newList = [12, 30, 19, 26, 21]
  
 print(swapList(newList))
+
+# Another approach
+# The usage of * operand
+
+list = [1, 2, 3, 4]
+ 
+a, *b, c = list
+ 
+print(a)
+print(b)
+print(c)
