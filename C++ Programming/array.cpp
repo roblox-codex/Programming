3c3088d3a9a5b2ca93c63705270be9af9485e895
Digvijay Singh <107340811+roblox-codex@users.noreply.github.com>
2023-09-16 22:13:57 +0530

    Update array.cpp

--- C++ Programming/array.cpp
+++ C++ Programming/array.cpp
@@ -27,7 +27,14 @@ using namespace std;
 int main() {
     const int rows = 3;
     const int cols = 3;
-    int arr[rows][cols] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    int arr[rows][cols];
+
+    cout << "Enter " << rows << " rows and " << cols << " columns of elements for the 2D array:" << endl;
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            cin >> arr[i][j];
+        }
+    }
 
     return 0;
 }
