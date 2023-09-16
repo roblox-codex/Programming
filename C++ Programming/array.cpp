835a906867dd2d8f28afb2c35e1b71cbff7d4558
Digvijay Singh <107340811+roblox-codex@users.noreply.github.com>
2023-09-16 22:12:59 +0530

    Update array.cpp

--- C++ Programming/array.cpp
+++ C++ Programming/array.cpp
@@ -10,3 +10,18 @@ int main() {
 
     return 0;
 }
+
+
+// 2-d Array
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    const int rows = 3;
+    const int cols = 3;
+    int arr[rows][cols] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+    return 0;
+}
