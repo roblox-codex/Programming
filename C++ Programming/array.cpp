69b78671f0841753fddde93d2a4ab7c2b2f4e6c9
Digvijay Singh <107340811+roblox-codex@users.noreply.github.com>
2023-09-16 22:13:35 +0530

    Update array.cpp

--- C++ Programming/array.cpp
+++ C++ Programming/array.cpp
@@ -6,10 +6,16 @@ using namespace std;
 
 int main() {
     const int size = 5;
-    int arr[size] = {1, 2, 3, 4, 5};
+    int arr[size];
+
+    cout << "Enter " << size << " elements for the 1D array:" << endl;
+    for (int i = 0; i < size; ++i) {
+        cin >> arr[i];
+    }
 
     return 0;
 }
+
 
 
 // 2-d Array
