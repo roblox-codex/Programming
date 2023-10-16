83f21bfa6b5e9f39248cf8d0ce89fdddd58c3cbd
Digvijay Singh <107340811+roblox-codex@users.noreply.github.com>
2023-10-17 00:52:44 +0530

    Create data_type.py

---
+++ Python/Unit-3/data_type.py
@@ -0,0 +1,37 @@
+# Integer
+integer_variable = 42
+
+# Float
+float_variable = 3.14
+
+# String
+string_variable = "Hello, World!"
+
+# List
+list_variable = [1, 2, 3, 4, 5]
+
+# Tuple
+tuple_variable = (10, 20, 30)
+
+# Dictionary
+dictionary_variable = {"name": "John", "age": 30, "city": "New York"}
+
+# Set
+set_variable = {1, 2, 3, 4, 5}
+
+# Boolean
+boolean_variable = True
+
+# None
+none_variable = None
+
+# Display the data types of the variables
+print(f"Integer: {type(integer_variable)}")
+print(f"Float: {type(float_variable)}")
+print(f"String: {type(string_variable)}")
+print(f"List: {type(list_variable)}")
+print(f"Tuple: {type(tuple_variable)}")
+print(f"Dictionary: {type(dictionary_variable)}")
+print(f"Set: {type(set_variable)}")
+print(f"Boolean: {type(boolean_variable)}")
+print(f"None: {type(none_variable)}")
