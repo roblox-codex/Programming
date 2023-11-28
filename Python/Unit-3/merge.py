1dd58b23c794d77a4ad0e0667bc60cc76597481b
Digvijay Singh <107340811+roblox-codex@users.noreply.github.com>
2023-11-28 16:09:38 +0530

    Create merge.py

---
+++ Python/Unit-3/merge.py
@@ -0,0 +1,47 @@
+def merge_sort(arr):
+    if len(arr)>1:
+
+        mid = len(arr)//2
+
+        L = arr[:mid]
+        R = arr[mid:]
+
+        merge_sort(L)
+        merge_sort(R)
+
+        i=j=k=0
+
+        while i< len(L) and j<len(R):
+            if L[i] <= R[j]:
+                arr[k] = L[i]
+                i+=1
+            else:
+                arr[k] = R[j]
+                j +=1
+            k +=1
+
+
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+
+
+def printList(arr):
+    for i in range(len(arr)):
+        print(arr[i], end=" ")
+    print()
+
+
+if __name__ == '__main__':
+    arr = list(map(int, input("Enter integers with space:- ").split()))
+    print("Given array is:- ")
+    printList(arr)
+    merge_sort(arr)
+    print("Sorted array is:- ")
+    printList(arr)
