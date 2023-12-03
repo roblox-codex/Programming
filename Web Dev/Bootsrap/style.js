aff195e066de102c6103a0e83d02e70a7c26bf6d
Digvijay Singh <107340811+roblox-codex@users.noreply.github.com>
2023-12-04 01:22:17 +0530

    Create style.js

---
+++ Web Dev/Bootsrap/style.js
@@ -0,0 +1,52 @@
+body, html {
+    height: 100%;
+    margin: 0;
+}
+
+.custom-container1 {
+    background-color: rgb(244, 185, 102);
+    height: 100%;
+    display: flex;
+    align-items: center;
+    padding-left: 10px;
+}
+
+.custom-container2 {
+    background-color: rgb(205, 246, 174);
+    height: 100%;
+    display: flex;
+    align-items: left;
+}
+
+.custom-container3 {
+    background-color: #ffffff;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+.custom-container4 {
+    background-color: #c5ced2;
+    height: 100%;
+    display: flex;
+    align-items: flex-end;
+    justify-content: flex-end;
+    padding-right: 15px;
+    padding-bottom: 15px;
+}
+
+.custom-segment {
+    background-color: #a4e6f5;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: left;
+    justify-content: left;
+}
+
+h1, p, li {
+    margin-left: 10px;
+    margin-right: 10px;
+}
