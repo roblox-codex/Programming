f9e9c77cf64bede4bf41e79637c49391891f3c1d
Digvijay Singh <107340811+roblox-codex@users.noreply.github.com>
2024-03-26 11:38:39 +0530

    Create rectangle_area.java

---
+++ Advance Java/rectangle_area.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class RectangleArea {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in); 
+
+    System.out.print("Enter the length of the rectangle: ");
+    double length = input.nextDouble(); 
+
+    System.out.print("Enter the width of the rectangle: ");
+    double width = input.nextDouble();
+
+    double area = length * width; 
+
+    System.out.println("The area of the rectangle is: " + area);
+  }
+}
