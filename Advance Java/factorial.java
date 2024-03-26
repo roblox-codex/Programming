b9c2710557c03ddec6ef58ffd346df2ed311cf6c
Digvijay Singh <107340811+roblox-codex@users.noreply.github.com>
2024-03-26 11:39:02 +0530

    Create factorial.java

---
+++ Advance Java/factorial.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        int factorial = 1;
+
+        for (int i = 1; i <= num; ++i) {
+            factorial *= i;
+        }
+
+        System.out.println("Factorial of " + num + " = " + factorial);
+    }
+}
